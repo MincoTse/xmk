68b0ec901404e06cf8dadade82ce3d1feb28b758
MincoTse <xiemingke@53zaixian.com>
2020-12-03 14:29:36 +0800

    初始化

---
+++ xmk-bsf-all/xmk-bsf-jetcache/src/test/java/com/xmk/bsf/jetcache/XmkBsfJetcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmk.bsf.jetcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmkBsfJetcacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
